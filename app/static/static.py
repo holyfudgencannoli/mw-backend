5010d57a36fa6249e3b40c7730a600e3fa460eab
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-07-31 18:08:51 -0400

    Delete app directory

--- app/static/static.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, send_from_directory, current_app
-
-
-app_bp = Blueprint('app', __name__)
-
-@app_bp.route('/')
-def homePage():
-    return send_from_directory(current_app.static_folder, "index.html")
